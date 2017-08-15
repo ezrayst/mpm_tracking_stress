9049f6b90dea0c097836d7dc3703de7b1e6c5d58
Ezra Tjung <ezrayst@berkeley.edu>
2017-08-14 17:37:36 -0700

    update for Krishna and Shyamini

--- main.cpp
+++ main.cpp
@@ -15,22 +15,25 @@ int main() {
     //! Reading input file
     //! Error would be reported
 
+    //! 1 for Krishna's code, and 0 for Shyamini's code
+    bool mpm_code = 1;
+
     //! Initialize a vector stress to contain the stress of a point
     std::vector<std::array<double, 3>> stress;
     std::array<double, 3> coordinates;
     std::vector<double> time_step;
     unsigned total_num_points;
-    const unsigned point_id = 0;
-    const unsigned ntime = 100;
+    const unsigned point_id = 790;
+    const unsigned ntime = 5000;
 
     //! User input inputFilename and outputFilename
     std::string foldername;
     std::string inputfilename;
     std::string outputfilename;
 
-    std::cout << "Type the working folder, default: [bin/element_testing/]: ";
+    std::cout << "Type the working folder, default: [bin/column_simple_k/]: ";
     std::getline(std::cin, foldername);
-    if (foldername == "") foldername = "bin/element_testing/";
+    if (foldername == "") foldername = "bin/column_simple_k/";
 
     //! Get output file name
     outputfilename = foldername + "stress_" + std::to_string(point_id) + ".txt";
@@ -39,17 +42,28 @@ int main() {
     for (unsigned t = 0; t <= ntime; ++t) {
 
       //! Get vector time_step that contains the time index
-      time_step.push_back(t * 10000);
+      time_step.push_back(t * 1000);
 
       //! Get inputfilename
-      if (t < 10) {
-        inputfilename = foldername + "Stress00" + std::to_string(t) + "0000.vtk";
-      } else if (t < 100) {
-        inputfilename = foldername + "Stress0" + std::to_string(t) + "0000.vtk";
-      } else if (t < 1000) {
-        inputfilename = foldername + "Stress" + std::to_string(t) + "0000.vtk";
+      if (mpm_code) {
+
+        //! Use below for Krishna's code
+        if (t < 10) {
+          inputfilename = foldername + "Stress000" + std::to_string(t) + "000.vtk";
+        } else if (t < 100) {
+          inputfilename = foldername + "Stress00" + std::to_string(t) + "000.vtk";
+        } else if (t < 1000) {
+          inputfilename = foldername + "Stress0" + std::to_string(t) + "000.vtk";
+        } else {
+          inputfilename = foldername + "Stress" + std::to_string(t) + "000.vtk";
+        }
       } else {
-        inputfilename = foldername + "Stress" + std::to_string(t) + "000.vtk";
+        //! Use below for Shyamini's code
+        if (t == 0) {
+          inputfilename = foldername + "stress" + std::to_string(t) + ".vtk";
+        } else {
+          inputfilename = foldername + "stress" + std::to_string(t) + "000.vtk";
+        }
       }
 
       //! Open input file and store x-dir
@@ -61,9 +75,18 @@ int main() {
       char unused_char;
       std::string unused_lines;
       std::array<double, 3> current_stress;
+      unsigned skip_char;
 
-      //! Loop through the unused characters
-      for (unsigned i = 0; i < 66; ++i) {
+      if (mpm_code) {
+        //! Use below for Krishna's code
+        skip_char = 66;
+      } else {
+        //! Use below for Shyamini's code
+        skip_char = 78;
+      }
+
+      //! Loop through the unused characters     
+      for (unsigned i = 0; i < skip_char; ++i) {
         inputFile >> unused_char;
       }
 
@@ -102,7 +125,7 @@ int main() {
 
       inputFile.close();
       std::cout << "The input file for time step: " << std::to_string(t)
-                << "0000 has been read."
+                << "000 has been read."
                 << "\n";
     }
 
